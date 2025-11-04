58b7fa008ed60976cffd093981a26fdd67b9ef0f
condrad otim <condradotim199@gmail.com>
2025-11-04 23:21:33 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+// src/App.tsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import LandingPage from "./screens/LandingPage";
 import Calculator from "./screens/Calculator";
+
+const LandingWrapper: React.FC = () => {
+  const navigate = useNavigate();
+  return <LandingPage onStart={() => navigate("/calculator")} />;
+};
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<LandingWrapper />} />
         <Route path="/calculator" element={<Calculator />} />
       </Routes>
     </Router>
